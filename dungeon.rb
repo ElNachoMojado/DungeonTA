4b9931bc4f8410b14e902212f5a85f466c71f011
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-04-03 23:37:29 +0100

    Add method add_room in the class Dungeon to create rooms

--- dungeon.rb
+++ dungeon.rb
@@ -5,6 +5,10 @@ class Dungeon
     @player = player
     @rooms = {}
   end
+
+  def add_room(reference, name, description, connections)
+    @rooms[reference] = Room.new(reference, name, description, connections)
+  end
 end
 
 Player = Struct.new(:name, :location)
@@ -12,4 +16,6 @@ Room = Struct.new(:reference, :name, :description, :connections)
 
 me = Player.new("Dany")
 calab = Dungeon.new(me)
+calab.add_room(:largecave, "Large Cave", "a large cavernous cave", {west: :smallcave})
+calab.add_room(:smallcave, "Small cave", "a small, claustrophobic cave", {east: :largecave})
 puts calab.player.name
